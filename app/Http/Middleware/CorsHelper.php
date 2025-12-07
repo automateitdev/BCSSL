d82eaa86f6b894d81d0e0d9be6461f2002ca3575
sft0595 <shafayet.rabby@gmail.com>
2025-12-08 00:28:40 +0600

    updating spg payment to v2

--- app/Http/Middleware/CorsHelper.php
+++ app/Http/Middleware/CorsHelper.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class CorsHelper
 {
@@ -11,12 +10,38 @@ class CorsHelper
     {
         $response = $next($request);
 
-        if ($response instanceof Response) {
-            $response->header("Access-Control-Allow-Origin", "*")
-                ->header("Access-Control-Allow-Methods", "*")
-                ->header("Access-Control-Allow-Headers", "*");
+        // If the response is not a proper response object, wrap it
+        if (!$response instanceof SymfonyResponse) {
+            $response = response($response);
         }
+
+        // Add CORS headers
+        $response->headers->set("Access-Control-Allow-Origin", "*");
+        $response->headers->set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        $response->headers->set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
         return $response;
     }
 }
+
+
+// namespace App\Http\Middleware;
+
+// use Closure;
+// use Illuminate\Http\Response;
+
+// class CorsHelper
+// {
+//     public function handle($request, Closure $next)
+//     {
+//         $response = $next($request);
+
+//         if ($response instanceof Response) {
+//             $response->header("Access-Control-Allow-Origin", "*")
+//                 ->header("Access-Control-Allow-Methods", "*")
+//                 ->header("Access-Control-Allow-Headers", "*");
+//         }
+
+//         return $response;
+//     }
+// }
