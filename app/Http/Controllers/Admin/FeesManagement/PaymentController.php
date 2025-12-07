4b48643a1da1e63abf69d5561a6064f36385c3f6
sft0595 <shafayet.rabby@gmail.com>
2025-12-08 00:24:01 +0600

    updating spg payment to v2

--- app/Http/Controllers/Admin/FeesManagement/PaymentController.php
+++ app/Http/Controllers/Admin/FeesManagement/PaymentController.php
@@ -327,8 +327,7 @@ class PaymentController extends Controller
 
     public function handlePayFlexVerification(Request $request)
     {
-
-
+        dd($request->all());
         Log::alert('HITS HANDLE PAYFLEX:::::::::::', [$request->all()]);
         // $data = $request->all();
         // // Check if PayFlex has provided data
