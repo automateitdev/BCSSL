45a80f2219277b0ad7a69ea19360546cdd196eaa
sft0595 <shafayet.rabby@gmail.com>
2025-12-08 03:01:59 +0600

    updating spg payment to v2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,13 +15,13 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        // \App\Http\Middleware\CorsHelper::class
+        \App\Http\Middleware\CorsHelper::class
     ];
 
     /**
@@ -42,8 +42,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
--- app/Http/Middleware/CorsHelper.php
+++ app/Http/Middleware/CorsHelper.php
@@ -2,7 +2,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Illuminate\Http\Response;
 
 class CorsHelper
 {
@@ -10,38 +10,12 @@ class CorsHelper
     {
         $response = $next($request);
 
-        // If the response is not a proper response object, wrap it
-        if (!$response instanceof SymfonyResponse) {
-            $response = response($response);
+        if ($response instanceof Response) {
+            $response->header("Access-Control-Allow-Origin", "*")
+                ->header("Access-Control-Allow-Methods", "*")
+                ->header("Access-Control-Allow-Headers", "*");
         }
-
-        // Add CORS headers
-        $response->headers->set("Access-Control-Allow-Origin", "*");
-        $response->headers->set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        $response->headers->set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
         return $response;
     }
 }
-
-
-// namespace App\Http\Middleware;
-
-// use Closure;
-// use Illuminate\Http\Response;
-
-// class CorsHelper
-// {
-//     public function handle($request, Closure $next)
-//     {
-//         $response = $next($request);
-
-//         if ($response instanceof Response) {
-//             $response->header("Access-Control-Allow-Origin", "*")
-//                 ->header("Access-Control-Allow-Methods", "*")
-//                 ->header("Access-Control-Allow-Headers", "*");
-//         }
-
-//         return $response;
-//     }
-// }
